7d7ab6c625c2ee681718c8ba55fa08ecc84eadb7
Hans Schroeder <hajschroeder@gmail.com>
2022-05-05 12:30:42 -0700

    first test written. Red

---
+++ spec/definer_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require 'definer'
+
+describe '#Word' do 
+
+describe '.all' do
+  it("returns an empty array when there are no words") do 
+    expect(Album.all).to eq([])
+  end
+end
+end
