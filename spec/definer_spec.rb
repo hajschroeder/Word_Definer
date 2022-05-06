e84bad8b058a15d6dfae64e147b19afb63683211
Hans Schroeder <hajschroeder@gmail.com>
2022-05-06 10:09:00 -0700

    include second test. Red

--- spec/definer_spec.rb
+++ spec/definer_spec.rb
@@ -3,9 +3,19 @@ require 'definer'
 
 describe '#Word' do 
 
-describe '.all' do
-  it("returns an empty array when there are no words") do 
-    expect(Album.all).to eq([])
+  describe '.all' do
+    it("returns an empty array when there are no words") do 
+      expect(Word.all).to eq([])
+    end
   end
-end
+
+  describe '#save' do
+    it("saves a word") do
+      word = Word.new("foo", nil)
+      word.save()
+      word_two = Word.new("bar", nil)
+      word_two.save()
+      expect(Word.all).to eq([word, word_two])
+    end
+  end
 end
