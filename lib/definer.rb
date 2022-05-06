29de0a5e0b7023614a2923ee3370c293972e9bed
Hans Schroeder <hajschroeder@gmail.com>
2022-05-06 10:18:07 -0700

    Include code for #save and #compare. Green

--- lib/definer.rb
+++ lib/definer.rb
@@ -1,3 +1,22 @@
 class Word
-  
+  attr_reader :id, :word
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(word, id)
+    @word = word
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all()
+    @@words.values
+  end
+
+  def save
+    @@words[self.id] = Word.new(self.word, self.id)
+  end
+
+  def ==(word_compare) 
+    self.word() == word_compare.word()
+  end
 end
